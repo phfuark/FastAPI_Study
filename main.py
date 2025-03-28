f4e41b83c70c062cd0bc6b2cc93d78c1705f166a
phfuark <paulohsilva2007@gmail.com>
2025-03-28 10:36:30 -0300

    First Commit
    CRUD: get product | create product

---
+++ main.py
@@ -0,0 +1,68 @@
+from typing import List
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+DATABASE_URL = 'sqlite:///./database.db'
+
+# Criação da engine do SQLAlchemy
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float, index=True)
+    description = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str
+    price: float
+
+class ProductResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+
+    # Essa configuração permite que o Pydantic leia os dados do modelo SQLAlchemy
+    class Config:
+        from_attributes = True
+
+
+@app.get('/get_product/', response_model=List[ProductResponse])
+def read_users(skip: int = 0, limit = 10, db: Session = Depends(get_db)):
+    users = db.query(Product).offset(skip).limit(limit).all()
+    return users
+
+@app.post('/create_product/', response_model=ProductResponse)
+def create_product(product: ProductCreate, db: Session = Depends(get_db)):
+    db_product = Product(name=product.name, description=product.description, price=product.price)
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
